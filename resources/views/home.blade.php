f925299b5967d572797a24674c7142c120e46030
JorgeLuix <tec.jorge.29@gmai.com>
2023-05-23 11:46:06 +0200

    modifiche

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,19 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="container">
+    <div class="card-container">
         <div class="row">
     @foreach ($comics as $comic)
-        <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card mb-3">
-                <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" class="card-image">
+        <div class="col-12 col-md-6 col-lg-2 mb-4">
+            <div class="jc-card">
+                <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" class="">
 
-                <div class="card-body">
+                <div class="cards-body">
                     <h2 class="card-title">{{$comic['title']}}</h2>
                     {{-- <p class="card-description">{{$comic['description']}}</p> --}}
+                    <div class="price-play">
                     <p class="card-text">{{$comic['price']}}</p>
+                     </div>
                 </div>
             </div>
         </div>
@@ -19,3 +21,65 @@
     </div>
 </div>
 @endsection
+
+<style lang="scss">
+    .card-container {
+  display: flex;
+  justify-content: center;
+ }
+    .jc-card {
+      width: 100%;
+      transition: all ease 0.4s;
+      border-radius: 5px;
+
+      .cards-body {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        padding: 1rem;
+        color: white;
+      }
+      .card-title {
+        font-size: 1rem;
+        font-weight: bold;
+        margin-bottom: 0.5rem;
+        white-space: wrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        text-align: center;
+      }
+
+      .price {
+        font-size: 1.2rem;
+        font-weight: bold;
+        text-align: center;
+      }
+
+    }
+    .jc-card:hover{
+        cursor: pointer;
+        background-color:  #252525;
+      }
+      .jc-card img{
+        width: 100%;
+        border-radius: 8px;
+        padding-top: 8px;
+      }
+      .price-play {
+        position: relative;
+      }
+      .price{
+        position: absolute;
+        right: 10px;
+        bottom: 10px;
+        padding: 18px;
+        background-color: #99E32D;
+        border-radius: 100%;
+        opacity: 0;
+        transition: all ease 0.4s;
+      }
+      .jc-card:hover .price-play .price{
+        opacity: 1;
+        transform: translateY(-20px);
+      }
+    </style>
